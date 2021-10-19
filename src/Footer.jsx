b27f02a83de019da586cb75df2ccc8e29edb2a71
gloe2019 <gloedem@gmail.com>
2021-10-19 11:31:36 -0400

    add navigation via materialUI + routing

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,13 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import Archive from '@material-ui/icons/Archive';
 import History from '@material-ui/icons/History';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
-  const [value, setValue] = useState(0)
+  const [ value, setValue ] = useState(0);
   return (
     <footer>
       <BottomNavigation
@@ -13,8 +15,8 @@ const Footer = () => {
         onChange={(event, newValue) => setValue(newValue)}
         showLabels
       >
-        <BottomNavigationAction label="Call History" icon={<History />}/>
-        <BottomNavigationAction label="Archived" icon={ <Archive /> }/>
+        <BottomNavigationAction label="Call History" value="/history" icon={<History />} component={Link} to="/history"/>
+        <BottomNavigationAction label="Archive" icon={ <Archive /> } value="/archive" component={Link} to="/archive"/>
       </BottomNavigation>
     </footer>
   );
