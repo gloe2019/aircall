33dda37e0738d813ec135db7d8528b17561cdd92
gloe2019 <gloedem@gmail.com>
2021-10-19 15:54:28 -0400

    filter activity based on is_archived state

--- src/components/ActivityFeed.jsx
+++ src/components/ActivityFeed.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
-
 import Activity from './Activity.jsx'
 const ActivityFeed = (props) => {
-  console.log(props)
-  const {activities} = props
-  const activityList = activities.map(activity => {
-    return (<Activity key={activity.id} {...activity}></Activity>)
+  // console.log(props)
+
+  const { activities, type, archiveCall } = props
+  console.log(activities)
+  const unarchivedActivity = activities.filter(activity => (activity.is_archived === false ))
+  const unarchivedList = unarchivedActivity.map((activity) => {
+    return (<Activity key={activity.id} {...activity} archiveCall={archiveCall}></Activity>)
   })
+  const archivedActivity= activities.filter(activity => (activity.is_archived === true))
+  
+  const archivedList = archivedActivity.map(activity => {
+    return (<Activity key={activity.id} {...activity} archiveCall={archiveCall}></Activity>)
+  })
+  console.log('..archived', archivedList)
+  console.log('..unarchived', unarchivedList)
   return (
     <div>
-      Feed Me!
-      {activityList}
+      {type === 'unarchived' && unarchivedList}
+      {type === 'archived' && archivedList}
     </div>)
 }
 
