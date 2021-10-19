924f2e19a129a5d1bea9fa09fb6591cf6a6f4afc
gloe2019 <gloedem@gmail.com>
2021-10-18 20:09:52 -0400

    add components, pass props from custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './Header.jsx';
+import Footer from './Footer.jsx'
+import ActivityFeed from './components/ActivityFeed.jsx';
+
+import useAppData from './hooks/useAppData.js';
 
 const App = () => {
+  const { activities, archiveCall} = useAppData();
   return (
     <div className='container'>
       <Header/>
-      <div className="container-view">Some activities should be here</div>
+      <div className="container-view">
+        <ActivityFeed activities={activities} archiveCall={archiveCall}/>
+      </div>
+      <Footer />
     </div>
   );
 };
